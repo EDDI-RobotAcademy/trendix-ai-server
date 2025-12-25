6a6dda585c4df8ee366255e48b22ab2fdcead96a
EarlyMorning <memory2520@gmail.com>
2025-12-25 11:12:03 +0900

    feat: [조범수]  불용어 처리 1차 적용

--- content/infrastructure/repository/stopword_repository_impl.py
+++ content/infrastructure/repository/stopword_repository_impl.py
@@ -3,14 +3,23 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 from config.database.session import SessionLocal
-from content.application.port.stopword_repository_port import StopwordRepositoryPort
 from content.infrastructure.orm.models import (
     StopwordORM
 )
 
-class StopwordRepositoryImpl(StopwordRepositoryPort):
+class StopwordRepositoryImpl():
+    
+    # 클래스 변수로 선언
+    __instance = None
+
     def __init__(self):
         self.db = SessionLocal()
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
 
     def get_stopwords(self, lang: str = "ko") -> Set[str]:
         try:
